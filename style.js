956dac384694bfe3c6189f862c74790106253021
Nguyễn Ngọc Phương Thảo <ThaoNNP3@fo.com>
2019-10-16 15:56:32 +0700

    update

---
+++ style.js
@@ -0,0 +1,13 @@
+
+    function onHover(){
+        $(".iconImg").attr('src', 'https://s.vnecdn.net/vnexpress/j/v130/event/longform/kinhdoanh/tafico/assets/images/answer5_5_active.png');
+        $(".iconImg").attr('style', 'border-color:#fff');
+        $(".box1").attr('style','border-color:#fff');
+    }
+
+    function offHover()
+    {
+        $(".iconImg").attr('src', 'https://s.vnecdn.net/vnexpress/j/v130/event/longform/kinhdoanh/tafico/assets/images/answer5_5.png');
+        $(".iconImg").attr('style', 'border-color: #02455c');
+        $(".box1").attr('style','border-color:#02455c');
+    }
